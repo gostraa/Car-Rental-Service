b083ef1d405945b0ba7bff508e094ece53ad44f8
gostraa <mcherkashina77@gmail.com>
2023-10-11 12:21:05 +0200

    code optimized in carsList

--- src/components/CarsList/CarsList.jsx
+++ src/components/CarsList/CarsList.jsx
@@ -35,7 +35,6 @@ const CarsList = () => {
   const dispatch = useDispatch();
 
   const location = useLocation();
-  console.log(location.pathname);
 
   useEffect(() => {
     dispatch(getPaginationAdvertThunk(currentPage));
@@ -64,161 +63,149 @@ const CarsList = () => {
     }
   };
 
-  if (location.pathname === '/favorites')
-    return (
-      favorites && (
-        <>
-          <StyledList>
-            {favorites.map(advert => {
-              const address = advert.address;
-              const parts = address.split(', ');
-              return (
-                <li key={advert.id}>
-                  <div
-                    style={{
-                      borderRadius: '12px',
-                      backgroundImage: `url(${advert.img})`,
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center',
-                      width: '274px',
-                      height: '268px',
-                      marginBottom: '14px',
-                      position: 'relative',
-                    }}
-                  >
-                    <FaRegHeart
+  return (
+    <>
+      {adverts.length !== 0 ? (
+        <StyledList>
+          {location.pathname === '/favorites'
+            ? favorites.map(advert => {
+                const address = advert.address;
+                const parts = address.split(', ');
+                return (
+                  <li key={advert.id}>
+                    <div
                       style={{
-                        position: 'absolute',
-                        top: '10px',
-                        right: '10px',
-
-                        border: 'none',
-                        color: '#3470FF',
-                        cursor: 'pointer',
-                        width: '23px',
-                        height: '23px',
+                        borderRadius: '12px',
+                        backgroundImage: `url(${advert.img})`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        width: '274px',
+                        height: '268px',
+                        marginBottom: '14px',
+                        position: 'relative',
                       }}
-                      onClick={() => handleClickToFavorites(advert)}
-                    />
-                  </div>
-                  <MainWrapper>
-                    <p>
-                      {advert.make.length > 8
-                        ? advert.make.slice(0, 8) + '...'
-                        : advert.make}
-                    </p>
-                    <p>
-                      {advert.model.length > 8
-                        ? advert.model.slice(0, 8) + '...'
-                        : advert.model}
-                    </p>
-                    <p>,{advert.year}</p>
-                    <p>{advert.rentalPrice}</p>
-                  </MainWrapper>
-                  <InfoWrapper>
-                    <p>{parts[parts.length - 2]}</p>
-                    <p>{parts[parts.length - 1]}</p>
-                    <p>{advert.rentalCompany}</p>
-                    <p>{advert.type}</p>
-                    <p>{advert.model}</p>
-                    <p>{advert.mileage}</p>
-                    <p>{advert.accessories[2]}</p>
-                  </InfoWrapper>
-                  <BtnLearnMore onClick={() => handleOpenModal(advert.id)}>
-                    Learn more
-                  </BtnLearnMore>
-                </li>
-              );
-            })}
-          </StyledList>
-          {isModalOpen && (
-            <CarInfoModal onClose={handleCloseModal} id={selectedId} />
-          )}
-        </>
-      )
-    );
-  if (location.pathname === '/catalog')
-    return (
-      <>
-        {adverts.length !== 0 ? (
-          <StyledList>
-            {adverts?.map(advert => {
-              const address = advert.address;
-              const parts = address.split(', ');
-              return (
-                <li key={advert.id}>
-                  <div
-                    style={{
-                      borderRadius: '12px',
-                      backgroundImage: `url(${advert.img})`,
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center',
-                      width: '274px',
-                      height: '268px',
-                      marginBottom: '14px',
-                      position: 'relative',
-                    }}
-                  >
-                    <FaRegHeart
-                      onClick={() => handleClickToFavorites(advert)}
+                    >
+                      <FaRegHeart
+                        style={{
+                          position: 'absolute',
+                          top: '10px',
+                          right: '10px',
+
+                          border: 'none',
+                          color: '#3470FF',
+                          cursor: 'pointer',
+                          width: '23px',
+                          height: '23px',
+                        }}
+                        onClick={() => handleClickToFavorites(advert)}
+                      />
+                    </div>
+                    <MainWrapper>
+                      <p>
+                        {advert.make.length > 8
+                          ? advert.make.slice(0, 8) + '...'
+                          : advert.make}
+                      </p>
+                      <p>
+                        {advert.model.length > 8
+                          ? advert.model.slice(0, 8) + '...'
+                          : advert.model}
+                      </p>
+                      <p>,{advert.year}</p>
+                      <p>{advert.rentalPrice}</p>
+                    </MainWrapper>
+                    <InfoWrapper>
+                      <p>{parts[parts.length - 2]}</p>
+                      <p>{parts[parts.length - 1]}</p>
+                      <p>{advert.rentalCompany}</p>
+                      <p>{advert.type}</p>
+                      <p>{advert.model}</p>
+                      <p>{advert.mileage}</p>
+                      <p>{advert.accessories[2]}</p>
+                    </InfoWrapper>
+                    <BtnLearnMore onClick={() => handleOpenModal(advert.id)}>
+                      Learn more
+                    </BtnLearnMore>
+                  </li>
+                );
+              })
+            : adverts.map(advert => {
+                const address = advert.address;
+                const parts = address.split(', ');
+                return (
+                  <li key={advert.id}>
+                    <div
                       style={{
-                        position: 'absolute',
-
-                        top: '10px',
-                        right: '10px',
-                        background: 'transparent',
-                        border: 'none',
-                        color: favorites.some(fav => fav.id === advert.id)
-                          ? '#3470FF'
-                          : 'white',
-                        cursor: 'pointer',
-                        width: '22px',
-                        height: '22px',
+                        borderRadius: '12px',
+                        backgroundImage: `url(${advert.img})`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        width: '274px',
+                        height: '268px',
+                        marginBottom: '14px',
+                        position: 'relative',
                       }}
-                    />
-                  </div>
-                  <MainWrapper>
-                    <p>
-                      {advert.make.length > 8
-                        ? advert.make.slice(0, 8) + '...'
-                        : advert.make}
-                    </p>
-                    <p>
-                      {advert.model.length > 8
-                        ? advert.model.slice(0, 8) + '...'
-                        : advert.model}
-                    </p>
-                    <p>,{advert.year}</p>
-                    <p>{advert.rentalPrice}</p>
-                  </MainWrapper>
-                  <InfoWrapper>
-                    <p>{parts[parts.length - 2]}</p>
-                    <p>{parts[parts.length - 1]}</p>
-                    <p>{advert.type}</p>
-                    <p>{advert.model}</p>
-                    <p>{advert.mileage}</p>
-                    <p>{advert.accessories[2]}</p>
-                  </InfoWrapper>
-                  <BtnLearnMore onClick={() => handleOpenModal(advert.id)}>
-                    Learn more
-                  </BtnLearnMore>
-                </li>
-              );
-            })}
-          </StyledList>
-        ) : (
-          <p>There's nothing here yet</p>
-        )}
+                    >
+                      <FaRegHeart
+                        onClick={() => handleClickToFavorites(advert)}
+                        style={{
+                          position: 'absolute',
 
-        {currentPage > 4 || adverts.length < 8 || (
+                          top: '10px',
+                          right: '10px',
+                          background: 'transparent',
+                          border: 'none',
+                          color: favorites.some(fav => fav.id === advert.id)
+                            ? '#3470FF'
+                            : 'white',
+                          cursor: 'pointer',
+                          width: '22px',
+                          height: '22px',
+                        }}
+                      />
+                    </div>
+                    <MainWrapper>
+                      <p>
+                        {advert.make.length > 8
+                          ? advert.make.slice(0, 8) + '...'
+                          : advert.make}
+                      </p>
+                      <p>
+                        {advert.model.length > 8
+                          ? advert.model.slice(0, 8) + '...'
+                          : advert.model}
+                      </p>
+                      <p>,{advert.year}</p>
+                      <p>{advert.rentalPrice}</p>
+                    </MainWrapper>
+                    <InfoWrapper>
+                      <p>{parts[parts.length - 2]}</p>
+                      <p>{parts[parts.length - 1]}</p>
+                      <p>{advert.type}</p>
+                      <p>{advert.model}</p>
+                      <p>{advert.mileage}</p>
+                      <p>{advert.accessories[2]}</p>
+                    </InfoWrapper>
+                    <BtnLearnMore onClick={() => handleOpenModal(advert.id)}>
+                      Learn more
+                    </BtnLearnMore>
+                  </li>
+                );
+              })}
+        </StyledList>
+      ) : (
+        <p>There's nothing here yet</p>
+      )}
+      {location.pathname !== '/catalog' ||
+        currentPage > 4 ||
+        adverts.length < 8 || (
           <BtnLoadMore onClick={handleLoadMore}>Load more</BtnLoadMore>
         )}
-
-        {isModalOpen && (
-          <CarInfoModal onClose={handleCloseModal} id={selectedId} />
-        )}
-      </>
-    );
+      {isModalOpen && (
+        <CarInfoModal onClose={handleCloseModal} id={selectedId} />
+      )}
+    </>
+  );
 };
 
 export default CarsList;
