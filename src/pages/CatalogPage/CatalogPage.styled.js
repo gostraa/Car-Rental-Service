58d775a61a3085045da7aa82c26c2eb8feb0c5ae
Mariia <mcherkashina77@gmail.com>
2023-11-28 15:11:53 +0100

    Update CatalogPage.styled.js

--- src/pages/CatalogPage/CatalogPage.styled.js
+++ src/pages/CatalogPage/CatalogPage.styled.js
@@ -1,12 +1,11 @@
 import styled from 'styled-components';
 
 export const Section = styled.section`
+  max-width: 1140px;
+  margin: 0 auto;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   padding: 70px 90px;
-  @media screen and (min-width: 1440px) {
-    padding: 70px 128px;
-  }
 `;
