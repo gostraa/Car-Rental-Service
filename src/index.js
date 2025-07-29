285a55e22c837211542f4c0966b4c44edb656eb2
Mariia <mcherkashina77@gmail.com>
2025-07-29 19:26:43 +0200

    Update BrowserRouter basename

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/technical_task">
+        <BrowserRouter basename="/car-rental-service">
           <App />
         </BrowserRouter>
       </PersistGate>
