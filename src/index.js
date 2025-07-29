f073903734a38890fab9778ba38c8a31015272f5
Mariia <mcherkashina77@gmail.com>
2025-07-29 19:33:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/car-rental-service">
+        <BrowserRouter basename="/car-rental-service/">
           <App />
         </BrowserRouter>
       </PersistGate>
